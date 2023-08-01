9ffdd7260ccd970019d161bcc428e21e9a64cd2e
devlopersabbir <devlopersabbir@gmail.com>
2023-08-01 22:06:34 +0600

    removed backgroundScript

--- src/background/background.ts
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message);
-  console.log(sender);
-  sendResponse({ snedMessage: "this is the onMessage response" });
-});

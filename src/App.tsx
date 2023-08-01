ec010482305c7706d585bf78f5002f1d5b8f730d
devlopersabbir <devlopersabbir@gmail.com>
2023-08-01 17:22:54 +0600

    ui almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,103 @@
 import {
-  Box,
-  Container,
   Flex,
   HStack,
   Heading,
-  IconButton,
+  Icon,
+  Text,
+  VStack,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { useState } from "react";
 import { MdSettings } from "react-icons/md";
+import {
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+  AiOutlineFullscreenExit,
+  AiOutlineRight,
+  AiOutlineLeft,
+  AiOutlinePlayCircle,
+} from "react-icons/ai";
+import Drawers from "./components/Drawer";
 
 const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const [isEyeOpen, setIsEyeOpen] = useState<boolean>(false);
   return (
-    <Flex
-      w="full"
-      h="100vh"
-      pos="relative"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDir="column"
-      bg="gray.800"
-    >
-      {/* for drawer */}
-      <HStack position="absolute" top={2} left={5}>
-        <IconButton
-          aria-label="setttings"
-          variant="unstyled"
-          color="white"
-          fontSize="2xl"
-          icon={<MdSettings />}
-        />
-      </HStack>
-      <Heading>20:30</Heading>
-      <Heading>Write something fun</Heading>
-      <Heading>Good Afternoon</Heading>
-    </Flex>
+    <>
+      <Flex
+        w="full"
+        h="100vh"
+        pos="relative"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDir="column"
+        // bg="gray.800"
+        bg="#1a202c33"
+      >
+        {/* for drawer */}
+        <Flex
+          position="absolute"
+          top={2}
+          left={5}
+          gap={3}
+          p={2}
+          rounded="full"
+          bg={!isShow ? "transparent" : "gray.600"}
+          transition="0.3s"
+          onMouseEnter={() => setIsShow(true)}
+          onMouseLeave={() => setIsShow(false)}
+        >
+          <Icon onClick={onOpen} color="white" fontSize="2xl" as={MdSettings} />
+
+          {isShow ? (
+            <HStack transition="0.3s">
+              <Icon
+                onClick={() => setIsEyeOpen(true)}
+                color="white"
+                fontSize="2xl"
+                as={isEyeOpen ? AiOutlineEyeInvisible : AiOutlineEye}
+              />
+              <Icon color="white" fontSize="2xl" as={AiOutlineFullscreenExit} />
+            </HStack>
+          ) : null}
+        </Flex>
+        <VStack color="white">
+          <Heading>20:30</Heading>
+          <Heading>Write something fun</Heading>
+          <Heading>Good Afternoon</Heading>
+        </VStack>
+        {/* bottom bar */}
+        <Flex
+          justify="center"
+          align="center"
+          flexDirection="row"
+          gap={2}
+          pos="absolute"
+          bottom={4}
+          w="full"
+          m="auto"
+        >
+          <Icon fontSize="md" cursor="pointer" as={AiOutlineLeft} />
+          <Icon fontSize="md" cursor="pointer" as={AiOutlinePlayCircle} />
+          <Icon fontSize="md" cursor="pointer" as={AiOutlineRight} />
+        </Flex>
+        {/* photo details */}
+        <Flex
+          justify="center"
+          align="center"
+          flexDirection="row"
+          gap={2}
+          pos="absolute"
+          bottom={4}
+          left={3}
+        >
+          <Text>Photo, Vadim Sherbakov, Unsplash</Text>
+        </Flex>
+      </Flex>
+      <Drawers isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 
--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Heading,
+} from "@chakra-ui/react";
+import "./scroll.css";
 
-const Drawers = () => {
-  return (
-    <div>Drawer</div>
-  )
+interface IDrawersProps {
+  isOpen: boolean;
+  onClose: () => void;
 }
 
-export default Drawers
+const Drawers = ({ isOpen, onClose }: IDrawersProps) => {
+  return (
+    <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay />
+      <DrawerContent
+        borderTopEndRadius="2xl"
+        borderBottomEndRadius="2xl"
+        bg="gray.600"
+        shadow="xl"
+      >
+        <DrawerBody
+          overflowY="scroll"
+          scrollBehavior="smooth"
+          w="full"
+          h="full"
+        >
+          <Flex flexDir="column" justify="flex-start">
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+              () => (
+                <Heading>LOGO</Heading>
+              )
+            )}
+          </Flex>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default Drawers;

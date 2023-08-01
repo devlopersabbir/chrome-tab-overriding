2fbd922bc1242cf448884a7d2cccfdd30f80e276
devlopersabbir <devlopersabbir@gmail.com>
2023-08-01 16:04:28 +0600

    working on home override page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import { Container, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  HStack,
+  Heading,
+  IconButton,
+} from "@chakra-ui/react";
+import { MdSettings } from "react-icons/md";
 
 const App = () => {
   return (
-    <Container maxW="400px" w="400px" p={4} mx="auto">
-      <Heading>Hello world</Heading>
-    </Container>
+    <Flex
+      w="full"
+      h="100vh"
+      pos="relative"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDir="column"
+      bg="gray.800"
+    >
+      {/* for drawer */}
+      <HStack position="absolute" top={2} left={5}>
+        <IconButton
+          aria-label="setttings"
+          variant="unstyled"
+          color="white"
+          fontSize="2xl"
+          icon={<MdSettings />}
+        />
+      </HStack>
+      <Heading>20:30</Heading>
+      <Heading>Write something fun</Heading>
+      <Heading>Good Afternoon</Heading>
+    </Flex>
   );
 };
 
---
+++ src/components/Drawer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Drawers = () => {
+  return (
+    <div>Drawer</div>
+  )
+}
+
+export default Drawers

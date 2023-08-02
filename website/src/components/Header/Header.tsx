8681fee52117d926091f646b2f831f1e81e9567a
devlopersabbir <devlopersabbir@gmail.com>
2023-08-02 15:24:07 +0600

    project setup done

--- website/src/components/Header/Header.tsx
+++ website/src/components/Header/Header.tsx
@@ -1,14 +1,41 @@
-import { Container, Heading, Stack, Text } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  Icon,
+  IconButton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { LuTwitter } from "react-icons/lu";
 
 const Header = () => {
   return (
-    <Container>
-      <Stack>
-        <Heading>LOGO</Heading>
-        <Text as={Link} to="/">
+    <Container maxW="container.sm" m="auto">
+      <Stack flexDir="row" gap={5} alignItems="center">
+        <Heading as={Link} to="/">
+          LOGO
+        </Heading>
+        <Text as={Link} to="/" fontSize="2xl" fontWeight="semibold">
           Home
         </Text>
+        <Text as={Link} to="/updates" fontSize="2xl" fontWeight="semibold">
+          Updates
+        </Text>
+        <Text as={Link} to="/Supports" fontSize="2xl" fontWeight="semibold">
+          Supports
+        </Text>
+        <IconButton
+          alignItems="center"
+          justifyContent="center"
+          fontSize="4xl"
+          variant="unstyled"
+          aria-label="twitter-btn"
+          icon={<LuTwitter />}
+          as={Link}
+          to="https://twitter.com/devlopersabbir"
+          target="_blank"
+        />
       </Stack>
     </Container>
   );
